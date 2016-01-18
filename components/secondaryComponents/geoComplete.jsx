6d5a996ec09d2344c8faadd6a5b513c4b09ba097
AlexanderSergan <sergana@edi.su>
2016-01-18 22:01:41 +0200

    added fezvrasta:material-design bootstrap theme

--- components/secondaryComponents/geoComplete.jsx
+++ components/secondaryComponents/geoComplete.jsx
@@ -1,43 +1,39 @@
 GeoComplete = React.createClass({
 
   getInitialState: function() {
-    return({
+    return ({
       userLocation: Meteor.user().profile.currentLocation || 'not defined'
     });
   },
 
   saveUserLocation: function(e) {
-    // Meteor.users.update({});
-    Meteor.users.update( { _id: Meteor.userId() },{$set: {'profile.currentLocation': this.state.userLocation}});
+    // Updating current user location in db
+    Meteor.users.update({ _id: Meteor.userId() },
+                        { $set: { 'profile.currentLocation': this.state.userLocation } });
     toastr.info(this.state.userLocation, 'Location set to');
   },
 
   componentDidMount: function() {
     var that = this;
+    // Initialisation of geo input and this.state.userLocation
     if (GoogleMaps.loaded()) {
-      $('#geoInput').geocomplete()
-          .bind("geocode:result", function(event, result){
-            // $('.geoResult').append('<span>' + "Result: " + result.formatted_address + '</span>');
-            // this.state.userLocation = result.formatted_address;
-            that.setState({
-              userLocation: result.formatted_address
-            });
-
-
-            console.info('userLocation set to ' + result.formatted_address +
-            '\nand the result is ' + JSON.stringify(result) );
-          });
+      $('#geoInput').geocomplete().bind("geocode:result", function(event, result) {
+        that.setState({userLocation: result.formatted_address});
+      });
     }
   },
 
   render: function() {
-    return(<div>
-      <input className='col-md-6' id='geoInput'/>
-      <h3>so chosen location is: <br />
-      <span className='geoResult'>{this.state.userLocation}</span></h3>
-      <div onClick={this.saveUserLocation} className='btn btn-default'>Save</div>
+    return (
+      <div>
+        <input className='col-md-9' id='geoInput'/>
+        <h3>So chosen location is:
+          <span className='geoResult'> {this.state.userLocation}</span>
+        </h3>
+        <div onClick={this.saveUserLocation} className='btn btn-default'>Update!</div>
 
-    </div>);
+      </div>
+    );
   }
 
 });
